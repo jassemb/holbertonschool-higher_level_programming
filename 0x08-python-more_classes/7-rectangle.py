fcd4536cbd21a1ec1bc3c1cc501938c0f33e85d3
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 <benalijassem201@gmail.com>
2021-10-04 15:51:08 -0700

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height of the class instance"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -40,7 +40,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets the width of the class instance"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -72,6 +72,7 @@ class Rectangle:
         """Returns the string representation of the class instance for
         recreation"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances += -1
