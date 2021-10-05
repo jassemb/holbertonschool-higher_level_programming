da294bd3c16f835e8ab7343e7e0c8aee9d643b3b
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 <benalijassem201@gmail.com>
2021-10-05 06:03:08 -0700

    matrix div

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines the divides of all elements in a matrix function
 """
+
+
 def matrix_divided(matrix, div):
     """ Function that div all element of matrix.
     Args:
@@ -8,18 +10,23 @@ def matrix_divided(matrix, div):
     Returns:
         list: New matrix
     """
-    if type(matrix) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     for row in matrix:
-        if type(row) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if not isinstance(row, list):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
     if len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     for row in matrix:
         for i in row:
-            if (type(i) != int and type(i) != float) or i != i:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if type(div) != int and type(div) != float:
+            if (not isinstance(i, int) and not isinstance(i, float)) or i != i:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of\
+                            integers/floats")
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div != div or div == float('inf'):
         raise TypeError("div must be a number")
@@ -28,11 +35,11 @@ def matrix_divided(matrix, div):
     a = len(matrix[0])
     for i in matrix:
         if a != len(i):
-            raise TypeError ("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix must have the same size")
     N_matrix = []
     for i in matrix:
-        N_list =[]
+        N_list = []
         for j in i:
-            N_list.append(round(j/div, 2))
+            N_list.append(round(j / div, 2))
         N_matrix.append(N_list)
     return N_matrix
