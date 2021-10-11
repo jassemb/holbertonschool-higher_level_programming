6d713b3d626906f5e8e573d36ab076837ec22f77
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 <benalijassem201@gmail.com>
2021-10-11 12:43:30 -0700

    a function that returns True if the object is an instance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ inherits_from module"""
 
+
 def inherits_from(obj, a_class):
     """function to verifie subclass"""
     if issubclass(obj.__class__, a_class) and type(obj) != a_class:
