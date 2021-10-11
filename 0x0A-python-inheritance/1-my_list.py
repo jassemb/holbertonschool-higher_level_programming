7d64cc6dd89f8df4ca5350c6dca45a8f87ceebcd
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 <benalijassem201@gmail.com>
2021-10-11 06:27:28 -0700

    sorted function

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+print_sorted function
+"""
+
+class MyList(list):
+    """MyList class"""
+
+    def print_sorted(self):
+        """
+        prints the list
+        """
+        print(sorted(self))
