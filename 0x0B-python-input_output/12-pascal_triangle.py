8201b3bde02db6c6ea332417c145c437be8133b6
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 <benalijassem201@gmail.com>
2021-10-12 15:14:22 -0700

    triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,16 +7,7 @@ Contains function that returns int lists of pascal triangle of any given size
 
 def pascal_triangle(n):
     """
-    Return:
-        empty list [] if n <= 0
-        if n is 7, we should expect:
-            [1]
-            [1, 1]
-            [1, 2, 1]
-            [1, 3, 3, 1]
-            [1, 4, 6, 4, 1]
-            [1, 5, 10, 10, 5, 1]
-            [1, 6, 15, 20, 15, 6, 1]
+    pritn pascal triangle
     """
     if n <= 0:
         return []
@@ -27,4 +18,4 @@ def pascal_triangle(n):
     for rows in range(n-1):
         triangle.append([a+b for a, b
                          in zip([0] + triangle[-1], triangle[-1] + [0])])
-    return triangl
+    return triangle
