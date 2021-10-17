1341f8640760824c41ad760ee7752bf01a888679
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 <benalijassem201@gmail.com>
2021-10-17 09:21:13 -0700

    square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""create square inherits from rectangle"""
+
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """
+    A class named Square
+    Attributes:
+        attr1(id): id of object
+        attr2(width): square width
+        attr3(height): square height
+        attr4(x): number of spaces before square
+        attr5(y): number of spaces before square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """instance of Square class"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation of Square class"""
+        return"[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width)
+    @property
+    def size(self):
+        """Returns the size of the object"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the object"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update attributes"""
+        attrs = ["id", "size", "x", "y"]
+        count = len(args)
+        if not args or count < 1:
+            for key, value in kwargs.items():
+                for i in range(len(attrs)):
+                    if key == attrs[i] and type(value) == int:
+                        setattr(self, attrs[i], value)
+        elif args and all(type(x) is int for x in args):
+            for i in range(count):
+                setattr(self, attrs[i], args[i])
+
