38fa99fbc4c47f90617a3a4385c1f80d8e7641d5
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 <benalijassem201@gmail.com>
2021-10-18 06:05:20 -0700

    to jeson

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """lists of dictionaries to json string"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return []
         return json.dumps(list_dictionaries)
 
