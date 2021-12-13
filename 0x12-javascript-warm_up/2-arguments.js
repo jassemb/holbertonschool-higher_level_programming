6e496d5e44309f911aa81756571c468b0e1ab747
jassemb <benalijassem201@gmail.com>
2021-12-13 13:17:07 +0100

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const l = args.length;
+if (l === 1) {
+  console.log('No argument');
+} else if (l === 2) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
