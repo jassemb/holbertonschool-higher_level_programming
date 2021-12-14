ad9ccb1efddb92c34fb87cde3365750a9e1b932d
jassemb <benalijassem201@gmail.com>
2021-12-14 12:56:12 +0100

    class rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
+  constructor (height, width) {
+    this.height = height;
+    this.width = width;
+  }
 };
