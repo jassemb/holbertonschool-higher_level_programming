12b537e284fe8686c74dedc2190927df42aa9a21
jassemb <benalijassem201@gmail.com>
2021-12-19 13:44:17 +0100

    model_state_fetch_first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
 """
-script that prints the first State object from
-the database hbtn_0e_6_usa
+A script lists all State objects from the database hbtn_0e_6_usa
 """
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
+from model_state import Base, State
 
-if __name__ == '__main__':
-    import model_state
-    from model_state import Base, State
-    # connect to the DB
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.
-        format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
-    # create a Session
-    mySession = Session()
-    # print(Session1.query(model_state.state).order_by(model_state.state.id).all())
-    mySessionQuery = mySession.query(model_state.State).filter(State.id == 1)
-
-    if mySessionQuery:
-        for st_obj in mySessionQuery:
-            print("{}: {}".format(st_obj.id, st_obj.name))
-    else:
-        print('Nothing\n')
-    mySession.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        print("{:}: ".format(session.query(State).order_by(
+            State.id).first().id), end="")
+        print(session.query(State).order_by(State.id).first().name)
+    except:
+        print("Nothing")
+    session.close()
