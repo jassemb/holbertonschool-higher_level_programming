ed8a67b6cec43de2b52a8c7cb68be2a69a156623
jassemb <benalijassem201@gmail.com>
2022-01-09 18:24:12 +0100

    hbt status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-Python script that fetches
-https://intranet.hbtn.io/status
+""" Python script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        print("Body response:")
         html = response.read()
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('utf-8'))
+    print("""
+            Body response:\n
+            \t- type: {}\n
+            \t- content: {}\n
+            \t- utf8 content: {}
+            """.format(type(html), html, html.decode("utf-8")))
