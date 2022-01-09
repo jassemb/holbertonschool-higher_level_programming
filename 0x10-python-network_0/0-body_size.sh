651a375279e0aa9165a9dbe66a9aba55ef25fdd5
jassemb <benalijassem201@gmail.com>
2022-01-09 03:00:55 +0100

    1-body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL sends a request to it, and displays the size of the body
-curl -w '%{size_download}\n' --silent --output /dev/null "$1"
+curl -sI "$1" | grep 'Content-Length:' | cut -c 17-
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# takes url, send a GET and display the body
+curl -sL GET "$1"
