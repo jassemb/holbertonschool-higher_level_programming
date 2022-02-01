a3200105c752c8610f972924e485cd02b27dd412
jassemb <benalijassem201@gmail.com>
2022-02-01 21:36:04 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-const argv = process.argv;
-const url = 'https://swapi-api.hbtn.io/api/films/';
-
-request.get(url + argv[2], function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
